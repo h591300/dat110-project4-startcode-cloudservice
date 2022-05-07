70afa0b428a61d3f7de3213c60fbf429e6d878e8
h591300 <h591300@stud.hvl.no>
2022-05-07 10:07:33 +0200

    Oppdatert

--- ACCloudService/src/main/java/no/hvl/dat110/ac/restservice/AccessLog.java
+++ ACCloudService/src/main/java/no/hvl/dat110/ac/restservice/AccessLog.java
@@ -10,6 +10,7 @@ public class AccessLog {
 	// atomic integer used to obtain identifiers for each access entry
 	private AtomicInteger cid;
 	protected ConcurrentHashMap<Integer, AccessEntry> log;
+	private Gson gson;
 	
 	public AccessLog () {
 		this.log = new ConcurrentHashMap<Integer,AccessEntry>();
@@ -19,27 +20,29 @@ public class AccessLog {
 	// TODO: add an access entry to the log for the provided message and return assigned id
 	public int add(String message) {
 		
-		int id = 0;
-		
+		int id = cid.incrementAndGet();
+		AccessEntry access = new AccessEntry(id, message);
+		log.put(id, access);
 		return id;
 	}
 		
 	// TODO: retrieve a specific access entry from the log
 	public AccessEntry get(int id) {
 		
-		return null;
+		return log.get(id);
 		
 	}
 	
 	// TODO: clear the access entry log
 	public void clear() {
-		
+		log.clear();
+		cid.set(0);
 	}
 	
 	// TODO: return JSON representation of the access log
 	public String toJson () {
     	
-		String json = null;
+		String json = gson.toJson(log);
     	
     	return json;
     }
--- ACCloudService/src/main/java/no/hvl/dat110/ac/restservice/App.java
+++ ACCloudService/src/main/java/no/hvl/dat110/ac/restservice/App.java
@@ -14,10 +14,10 @@ import com.google.gson.Gson;
  *
  */
 public class App {
-	
+
 	static AccessLog accesslog = null;
 	static AccessCode accesscode = null;
-	
+
 	public static void main(String[] args) {
 
 		if (args.length > 0) {
@@ -27,25 +27,81 @@ public class App {
 		}
 
 		// objects for data stored in the service
-		
+
 		accesslog = new AccessLog();
-		accesscode  = new AccessCode();
-		
+		accesscode = new AccessCode();
+
 		after((req, res) -> {
-  		  res.type("application/json");
-  		});
-		
+			res.type("application/json");
+		});
+
 		// for basic testing purposes
 		get("/accessdevice/hello", (req, res) -> {
-			
-		 	Gson gson = new Gson();
-		 	
-		 	return gson.toJson("IoT Access Control Device");
+
+			Gson gson = new Gson();
+
+			return gson.toJson("IoT Access Control Device");
 		});
-		
+
 		// TODO: implement the routes required for the access control service
 		// as per the HTTP/REST operations describined in the project description
-		
-    }
-    
+
+		// Skal returnere JSON-representasjon av alle loggene i systemet, altså alle som
+		// har forsøkt å få tilgang - en samling av typen AccessEntry.
+
+		get("/accessdevice/log", (req, res) -> {
+
+			return accesslog.toJson();
+		});
+
+		// Post lagrer når vi prøver å få tilgang, ved å lagre logg-meldingen som fins i
+		// body.
+
+		post("/accessdevice/log", (req, res) -> {
+
+			Gson gson = new Gson();
+
+			AccessMessage msg = gson.fromJson(req.body(), AccessMessage.class);
+			int id = accesslog.add(msg.getMessage());
+
+			return gson.toJson(accesslog.get(id));
+		});
+
+		// Returnerer JSON-representasjon av tilgang til gitt ID
+
+		get("/accessdevice/log/:id", (req, res) -> {
+			Gson gson = new Gson();
+
+			int id = Integer.parseInt(req.params(":id"));
+
+			return gson.toJson(accesslog.get(id));
+		});
+
+		// Setter ny accesscode i skytjenesten.
+
+		put("/accessdevice/code", (req, res) -> {
+			Gson gson = new Gson();
+
+			AccessCode kode = gson.fromJson(req.body(), AccessCode.class);
+			accesscode.setAccesscode(kode.getAccesscode());
+
+			return gson.toJson(kode);
+		});
+
+		// Henter accesskode fra server.
+
+		get("/accessdevice/code", (req, res) -> {
+			Gson gson = new Gson();
+
+			return gson.toJson(accesscode);
+		});
+
+		// Accesslogg blir sletta, returner body i HTTP response som tomt Json objekt
+
+		delete("/accessdevice/log", (req, res) -> {
+			accesslog.clear();
+			return accesslog.toJson();
+		});
+	}
+
 }
